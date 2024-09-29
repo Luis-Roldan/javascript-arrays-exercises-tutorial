5848edadc58c7e8238d26bda9c8bf2cd66fd7034
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-09-29 22:05:49 +0000

    1

---
+++ .learn/resets/01-Hello_World/app.js
@@ -0,0 +1,10 @@
+// console log hello world here
+
+const standardsMaker = () => {
+
+    for(let i = 0; i <= 300; i++){
+        console.log("I will write questions if I'm stuck" + i)
+    }
+};
+
+standardsMaker();
